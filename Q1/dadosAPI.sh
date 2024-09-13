4a82dbc2177345a7c181b176cac8585766db53df
Pedro-WB <rodripadidev@gmail.com>
2024-09-13 07:27:34 -0300

    Salvando Alterações

--- Q1/dadosAPI.sh
+++ Q1/dadosAPI.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Gere um número aleatório entre 1 e 100
 num=$((RANDOM % 100 + 1))
 
-# Faça a requisição para a API
 res=$(curl -s "https://dummyjson.com/user/$num")
 
-# Extraia os dados necessários
-nm=$(echo "$res" | grep -oP '"firstName":\s*"\K[^"]+')
-sn=$(echo "$res" | grep -oP '"lastName":\s*"\K[^"]+')
+nm=$(echo "$res" | grep -oP '"nome":\s*"\K[^"]+')
+sn=$(echo "$res" | grep -oP '"sobrenome":\s*"\K[^"]+')
 un=$(echo "$res" | grep -oP '"username":\s*"\K[^"]+')
 pw=$(echo "$res" | grep -oP '"password":\s*"\K[^"]+')
 ip=$(echo "$res" | grep -oP '"ip":\s*"\K[^"]+')
 em=$(echo "$res" | grep -oP '"email":\s*"\K[^"]+')
 
-# Combine os dados no formato desejado e imprima
 echo "$nm $sn::$un::$pw::$ip::$em"
 
